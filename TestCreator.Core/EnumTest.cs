d5ea0ef44c54e0b120d1c897d90b7e263f55fa58
e.raoufi <ErshadRaoufi@gmail.com>
2023-02-14 10:42:00 +0330

    add description for methods

--- TestCreator.Core/EnumTest.cs
+++ TestCreator.Core/EnumTest.cs
@@ -4,6 +4,12 @@ namespace TestCreator.Core
 {
     public static class EnumTest
     {
+        /// <summary>
+        /// Create Unit Test For Enums
+        /// </summary>
+        /// <param name="path">path of unitTest creation</param>
+        /// <param name="selectedAssembly">List Of Ass that you want Find Enums and Write Unit tests</param>
+        /// <exception cref="ArgumentException">when Entry data is invalid</exception>
         public static void WriteUnitTest(string path, string[] selectedAssembly)
         {
             if (string.IsNullOrEmpty(path))
@@ -15,18 +21,44 @@ namespace TestCreator.Core
             if (!selectedAssembly.Any())
                 throw new ArgumentException("selectedAssemly not given");
 
-            Helper.CreateUnitTestFile(path,selectedAssembly);
+            Helper.CreateUnitTestFile(path, selectedAssembly);
         }
-        
-       
-    }
-    public static class ServicesExtensions
-    {
-        public static void WriteUnitTest(this IServiceCollection services,string path, string[] selectedAssembly)
+
+        /// <summary>
+        /// Create Unit Test For Enums
+        /// </summary>
+        /// <param name="selectedAssembly">key : Assembly Name , Value : path of unitTest creation</param>
+        public static void WriteUnitTest(IDictionary<string, string> selectedAssembly)
         {
-            EnumTest.WriteUnitTest(path,selectedAssembly);
+            foreach (var assembly in selectedAssembly)
+            {
+                WriteUnitTest(assembly.Value, new[] { assembly.Key });
+            }
         }
     }
 
-   
+    public static class ServicesExtensions
+    {
+        /// <summary>
+        /// Create Unit Test For Enums
+        /// </summary>
+        /// <param name="services"></param>
+        /// <param name="path">path of unitTest creation</param>
+        /// <param name="selectedAssembly">List Of Ass that you want Find Enums and Write Unit tests</param>
+        /// <exception cref="ArgumentException">when Entry data is invalid</exception>
+        public static void WriteUnitTest(this IServiceCollection services, string path, string[] selectedAssembly)
+        {
+            EnumTest.WriteUnitTest(path, selectedAssembly);
+        }
+
+        /// <summary>
+        /// Create Unit Test For Enums
+        /// </summary>
+        /// <param name="services"></param>
+        /// <param name="selectedAssembly">key : Assembly Name , Value : path of unitTest creation</param>
+        public static void WriteUnitTest(this IServiceCollection services, IDictionary<string, string> selectedAssembly)
+        {
+            EnumTest.WriteUnitTest(selectedAssembly);
+        }
+    }
 }
